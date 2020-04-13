fd84cee8c5dc4c69006bfd0bea39c720da6ac283
Robin Steiner <robin95@bluewin.ch>
2020-04-13 15:32:34 +0200

    Add trash widget

---
+++ jobs/trash.rb
@@ -0,0 +1,21 @@
+require "pry"
+
+require 'net/http'
+require 'json'
+
+SCHEDULER.every '10s' do
+
+  # Grab paper dates
+  url = ENV ['TRASH_URL']
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+  text = response.body
+
+  trashArray = text.scan(/\w{2},\s\d{2}\.\s\w{3}.\s\d{4},\s\d{2}:\d{2}/)
+
+
+  send_event('trash', { trash: trashArray })
+end
